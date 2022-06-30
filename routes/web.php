bf2d5ef6def177bb152bb804e34a20785e0e7ab1
Michael <hegarty-m14@ulster.ac.uk>
2022-06-30 21:39:46 +0100

    Signed-off-by: Michael <hegarty-m14@ulster.ac.uk>

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//All Listings
+Route::get('/', [ListingController::class, 'index']);
+
+
+//Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+//Store Listing Data
+Route::post('/listings', [ListingController::class, 'store']); 
+
+//Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']); 
+
+//Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']); 
+
+//Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']); 
+
+
+//Single Listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']); 
